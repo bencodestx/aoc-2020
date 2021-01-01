2f4de9442581d401628b70d99779385fa91dd03c
Ben Goodwyn <bengoodwyn@me.com>
2021-01-01 15:34:59 -0600

    :arrow_right: Day 1 input
    Problem:
    - Input is not available to an algorithm
    
    Solution:
    - Read the input into a vector of integers

--- src/day1_part1_test.cpp
+++ src/day1_part1_test.cpp
@@ -1,7 +1,30 @@
 #include <boost/ut.hpp>
 
+#include <sstream>
+#include <vector>
+
 int main() {
   using namespace boost::ut;
 
-  "stub"_test = [] { expect(true); };
+  constexpr auto example_expense_report{
+      R"(1721
+979
+366
+299
+675
+1456)"};
+
+  static constexpr auto day1_input = [](std::istream &in) {
+    std::vector<int> values{};
+    int value;
+    while (in >> value) {
+      values.push_back(value);
+    }
+    return values;
+  };
+
+  "input"_test = [&] {
+    std::stringstream in(example_expense_report);
+    expect(std::vector{1721, 979, 366, 299, 675, 1456} == day1_input(in));
+  };
 }
