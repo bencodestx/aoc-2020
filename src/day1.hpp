7d570ebb05acd0f74edc1ea1d159692da03a3e7d
Ben Goodwyn <bengoodwyn@me.com>
2021-01-01 17:31:07 -0600

    :art: [day1] Make smaller functions
    Problem:
    - if constexpr made for one large function rather than two smaller functions
    - Partial function template specialization is not allowed
    
    Solution:
    - Capture the iterator type in an object
    - Use full template specialization for depth to split the implementation into two functions

--- src/day1.hpp
+++ src/day1.hpp
@@ -14,30 +14,33 @@ static constexpr auto day1_input = [](std::istream &in) {
   return values;
 };
 
-template <size_t Depth, typename Iter>
-std::optional<int> solve_day1(Iter begin, Iter end, const int target) {
-  if constexpr (Depth == 1) {
-    if (const auto it = std::find(begin, end, target); end != it) {
-      return *it;
-    } else {
-      return std::nullopt;
-    }
-  } else {
+template <typename Iter> struct day1_solver final {
+  template <size_t Depth>
+  std::optional<int> solve(Iter begin, Iter end, const int target) {
     for (auto it = begin; it != end; ++it) {
-      const auto child_answer =
-          solve_day1<Depth - 1>(it + 1, end, target - *it);
+      const auto child_answer = solve<Depth - 1>(it + 1, end, target - *it);
       if (child_answer.has_value()) {
         return *child_answer * *it;
       }
     }
     return std::nullopt;
   }
-}
+
+  template <>
+  std::optional<int> solve<1>(Iter begin, Iter end, const int target) {
+    if (const auto it = std::find(begin, end, target); end != it) {
+      return *it;
+    } else {
+      return std::nullopt;
+    }
+  }
+};
 
 template <size_t Depth>
 static constexpr auto day1 = [](const auto &expenses) {
-  const auto solution =
-      solve_day1<Depth>(std::begin(expenses), std::end(expenses), 2020);
+  day1_solver<decltype(std::end(expenses))> solver{};
+  const auto solution = solver.template solve<Depth>(std::begin(expenses),
+                                                     std::end(expenses), 2020);
   if (not solution.has_value()) {
     throw std::runtime_error("No solution found");
   }
