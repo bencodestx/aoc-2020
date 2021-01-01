240daa1c35c4041ad2a264f01fba248f064f7ab8
Ben Goodwyn <bengoodwyn@me.com>
2021-01-01 16:52:54 -0600

    :art: [day1] Refactor towards a more generic solution
    Problem:
    - Need to support searching for three expenses
    
    Solution:
    - Adjust the search algorithm to be generic based on the count of expenses to search for

--- src/day1.hpp
+++ src/day1.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <istream>
+#include <numeric>
 #include <vector>
 
 static constexpr auto day1_input = [](std::istream &in) {
@@ -13,15 +14,48 @@ static constexpr auto day1_input = [](std::istream &in) {
   return values;
 };
 
-static constexpr auto day1_part1 = [](const auto &expenses) {
-  for (auto it = std::begin(expenses); it != std::end(expenses); ++it) {
-    const auto second = std::find_if(it + 1, std::end(expenses),
-                                     [first = *it](const auto candidate) {
-                                       return first + candidate == 2020;
-                                     });
-    if (second != std::end(expenses)) {
-      return *it * *second;
+template <typename Iter, size_t Count> struct day1_search final {
+  day1_search(const auto &container) : end(std::end(container)) {
+    Iter it = std::begin(container);
+    for (auto &iterator : iterators) {
+      iterator = it++;
     }
   }
-  throw std::runtime_error("No solution found");
+
+  void next() {
+    size_t i = Count - 1;
+    while (true) {
+      if (end != ++iterators[i]) {
+        ++i;
+        while (i < Count) {
+          iterators[i] = iterators[i - 1] + 1;
+          if (end == iterators[i]) {
+            throw std::runtime_error("No solution found");
+          }
+          ++i;
+        }
+        return;
+      }
+      --i;
+    }
+  }
+
+  auto operator[](const size_t i) { return *iterators[i]; }
+
+  std::array<Iter, Count> iterators{};
+
+private:
+  const Iter end;
+};
+
+static constexpr auto day1_part1 = [](const auto &expenses) {
+  day1_search<decltype(std::end(expenses)), 2> search{expenses};
+  while (true) {
+    const auto first = search[0];
+    const auto second = search[1];
+    if (2020 == (first + second)) {
+      return first * second;
+    }
+    search.next();
+  }
 };
