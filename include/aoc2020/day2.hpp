7f287b1ec67b10623deeb2f656e9f34a826937cc
Ben Goodwyn <bengoodwyn@me.com>
2021-01-02 12:12:33 -0600

    :art: [day2] Extract day2 implementation to its own header

---
+++ include/aoc2020/day2.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <istream>
+#include <string>
+#include <vector>
+
+namespace aoc2020::day2 {
+using password_t = std::string;
+
+struct policy_t final {
+  ssize_t lowest{};
+  ssize_t highest{};
+  char letter{};
+
+  bool operator==(const policy_t &rhs) const {
+    return lowest == rhs.lowest and highest == rhs.highest and
+           letter == rhs.letter;
+  }
+
+  bool validate(const password_t &password) const {
+    const auto count =
+        std::count(std::begin(password), std::end(password), letter);
+    return count >= lowest and count <= highest;
+  }
+};
+
+struct password_and_policy_t final {
+  password_t password{};
+  policy_t policy{};
+
+  bool operator==(const password_and_policy_t &rhs) const {
+    return password == rhs.password and policy == rhs.policy;
+  }
+};
+
+using passwords_and_policies_t = std::vector<password_and_policy_t>;
+
+std::istream &operator>>(std::istream &is, policy_t &policy) {
+  char dash;
+  char colon;
+
+  if (is >> policy.lowest >> dash >> policy.highest >> policy.letter >> colon) {
+    assert(dash == '-');
+    assert(colon == ':');
+  }
+
+  return is;
+}
+
+std::istream &operator>>(std::istream &is, password_and_policy_t &row) {
+  is >> row.policy >> row.password;
+  return is;
+}
+
+auto input(std::istream &is) {
+  passwords_and_policies_t result{};
+  password_and_policy_t row;
+  while (is >> row) {
+    result.push_back(row);
+  }
+  return result;
+}
+
+auto part1(const auto &input) {
+  return std::count_if(std::begin(input), std::end(input), [](const auto &row) {
+    return row.policy.validate(row.password);
+  });
+}
+} // namespace aoc2020::day2
