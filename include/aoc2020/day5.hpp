f95473d1c7e9633431f5970b16ff7a75492b9008
Ben Goodwyn <bengoodwyn@me.com>
2021-01-07 21:17:44 -0600

    :new: [day5] Stub day5 files

---
+++ include/aoc2020/day5.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+namespace aoc2020::day5 {}
---
+++ src/day5_test.cpp
@@ -0,0 +1,10 @@
+#include "aoc2020/day5.hpp"
+
+#include <boost/ut.hpp>
+
+int main() {
+  using namespace aoc2020::day5;
+  using namespace boost::ut;
+
+  "not quite a"_test = [&] {};
+}
