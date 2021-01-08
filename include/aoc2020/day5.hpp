c417f3020e77911bac433737a7b0a940d6328d21
Ben Goodwyn <bengoodwyn@me.com>
2021-01-07 21:40:49 -0600

    :checkered_flag: [day5] Finished day5 part1

--- include/aoc2020/day5.hpp
+++ include/aoc2020/day5.hpp
@@ -1,3 +1,47 @@
 #pragma once
 
-namespace aoc2020::day5 {}
+#include <istream>
+#include <string>
+#include <string_view>
+#include <tuple>
+
+namespace aoc2020::day5 {
+using range_t = std::pair<size_t, size_t>;
+using location_t = std::pair<size_t, size_t>;
+
+range_t take(const char direction, const range_t x) {
+  if (direction == 'F' or direction == 'L') {
+    return {x.first, x.second - (x.second - x.first + 1) / 2};
+  } else {
+    return {x.first + (x.second - x.first + 1) / 2, x.second};
+  }
+}
+
+location_t locate(const std::string_view pass) {
+  range_t row{0, 127};
+  for (size_t i = 0; i < 7; ++i) {
+    row = take(pass[i], row);
+  }
+
+  range_t col{0, 7};
+  for (size_t i = 7; i < 10; ++i) {
+    col = take(pass[i], col);
+  }
+
+  return {row.first, col.first};
+};
+
+size_t seatid(const location_t location) {
+  return location.first * 8u + location.second;
+}
+
+size_t part1(std::istream &in) {
+  size_t highest{0u};
+  std::string pass;
+  while (in >> pass) {
+    highest = std::max(highest, seatid(locate(pass)));
+  }
+  return highest;
+}
+
+} // namespace aoc2020::day5
--- src/day5_test.cpp
+++ src/day5_test.cpp
@@ -6,5 +6,73 @@ int main() {
   using namespace aoc2020::day5;
   using namespace boost::ut;
 
-  "not quite a"_test = [&] {};
+  "F of 0-127 is 0-63"_test = [&] {
+    const auto result = take('F', {0, 127});
+    expect(result.first == 0_u);
+    expect(result.second == 63_u);
+  };
+
+  "B of 0-63 is 32-63"_test = [&] {
+    const auto result = take('B', {0, 63});
+    expect(result.first == 32_u);
+    expect(result.second == 63_u);
+  };
+
+  "F of 32-63 is 32-47"_test = [&] {
+    const auto result = take('F', {32, 63});
+    expect(result.first == 32_u);
+    expect(result.second == 47_u);
+  };
+
+  "B of 32-47 is 40-47"_test = [&] {
+    const auto result = take('B', {32, 47});
+    expect(result.first == 40_u);
+    expect(result.second == 47_u);
+  };
+
+  "B of 40-47 is 44-47"_test = [&] {
+    const auto result = take('B', {40, 47});
+    expect(result.first == 44_u);
+    expect(result.second == 47_u);
+  };
+
+  "F of 44-47 is 44-45"_test = [&] {
+    const auto result = take('F', {44, 47});
+    expect(result.first == 44_u);
+    expect(result.second == 45_u);
+  };
+
+  "F of 44-45 is 44-44"_test = [&] {
+    const auto result = take('F', {44, 45});
+    expect(result.first == 44_u);
+    expect(result.second == 44_u);
+  };
+
+  "R of 0-7 is 4-7"_test = [&] {
+    const auto result = take('R', {0, 7});
+    expect(result.first == 4_u);
+    expect(result.second == 7_u);
+  };
+
+  "L of 4-7 is 4-5"_test = [&] {
+    const auto result = take('L', {4, 7});
+    expect(result.first == 4_u);
+    expect(result.second == 5_u);
+  };
+
+  "R of 4-5 is 5-5"_test = [&] {
+    const auto result = take('R', {4, 5});
+    expect(result.first == 5_u);
+    expect(result.second == 5_u);
+  };
+
+  "FBFBBFFRLR is row 44, column 5"_test = [&] {
+    const auto result = locate("FBFBBFFRLR");
+    expect(result.first == 44_u);
+    expect(result.second == 5_u);
+  };
+
+  "row 44, column 5 has seat id 357"_test = [&] {
+    expect(357_u == seatid({44, 5}));
+  };
 }
