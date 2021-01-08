ca3bce9fced6735a2b293b0c0f96f20151d1ef15
Ben Goodwyn <bengoodwyn@me.com>
2021-01-07 22:24:16 -0600

    :checkered_flag: [day6] Completed day6 part1

--- include/aoc2020/day6.hpp
+++ include/aoc2020/day6.hpp
@@ -1,10 +1,28 @@
 #pragma once
 
+#include <algorithm>
+#include <bitset>
 #include <istream>
-
+#include <iterator>
+#include <string>
 namespace aoc2020::day6 {
 
-auto part1(std::istream &) { return "stub"; }
+auto part1(std::istream &in) {
+  std::string line;
+  std::bitset<26> yes{};
+  size_t sum{};
+  while (std::getline(in, line)) {
+    if (std::empty(line)) {
+      sum += yes.count();
+      yes.reset();
+    } else {
+      std::for_each(std::cbegin(line), std::cend(line), [&yes](const auto c) {
+        yes.set(static_cast<size_t>(c - 'a'));
+      });
+    }
+  }
+  return sum + yes.count();
+}
 
 auto part2(std::istream &) { return "stub"; }
 
--- src/day6_test.cpp
+++ src/day6_test.cpp
@@ -2,7 +2,22 @@
 
 #include <boost/ut.hpp>
 
+#include <sstream>
+
 int main() {
   using namespace aoc2020::day6;
   using namespace boost::ut;
+
+  "in this group there are 6 questions to which anyone answered yes"_test =
+      [&] {
+        std::stringstream example_group{"abcx\nabcy\nabcz"};
+        expect(6_u == part1(example_group));
+      };
+
+  "in this example the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11"_test =
+      [&] {
+        std::stringstream example_groups{
+            "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb"};
+        expect(11_u == part1(example_groups));
+      };
 }
