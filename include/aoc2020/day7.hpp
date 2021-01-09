88de0649af052e0d332d6a7dbba825ca64de8957
Ben Goodwyn <bengoodwyn@me.com>
2021-01-08 21:57:40 -0600

    :arrow_right: [day7] Day 7 input

--- include/aoc2020/day7.hpp
+++ include/aoc2020/day7.hpp
@@ -1,8 +1,71 @@
 #pragma once
 
 #include <istream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace aoc2020::day7 {
+
+struct contents_t final {
+  size_t count{};
+  std::string color{};
+
+  bool operator==(const contents_t &rhs) const {
+    return count == rhs.count and color == rhs.color;
+  }
+};
+
+using contents_list_t = std::vector<contents_t>;
+struct rule_t final {
+  std::string color{};
+  contents_list_t contents{};
+};
+
+using rules_t = std::unordered_map<std::string, rule_t>;
+
+std::istream &operator>>(std::istream &in, rule_t &rule) {
+  rule.color.clear();
+  rule.contents.clear();
+
+  std::string line{};
+  if (std::getline(in, line)) {
+    std::stringstream tokens{line};
+    std::string adjective{};
+    std::string color{};
+    tokens >> adjective >> color;
+    rule.color = adjective + " " + color;
+    std::string bags{};
+    std::string contain{};
+    tokens >> bags >> contain;
+    std::string trailer{};
+    do {
+      contents_t contents{};
+      std::string count{};
+      tokens >> count >> adjective >> color;
+      if (count != "no") {
+        contents.count = std::stoul(count);
+        contents.color = adjective + " " + color;
+        rule.contents.push_back(contents);
+        tokens >> trailer;
+      } else {
+        break;
+      }
+    } while (trailer == "bag," or trailer == "bags,");
+  }
+  return in;
+}
+
+auto input(std::istream &in) {
+  rules_t rules{};
+  rule_t rule{};
+  while (in >> rule) {
+    rules[rule.color] = rule;
+  }
+  return rules;
+}
 
 auto part1(std::istream &) { return "stub"; }
 
--- src/day7_test.cpp
+++ src/day7_test.cpp
@@ -4,9 +4,37 @@
 
 #include <sstream>
 
+const std::string example{
+    R"(light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.)"};
+
 int main() {
   using namespace aoc2020::day7;
   using namespace boost::ut;
 
-  "stub"_test = [] {};
+  "input"_test = [] {
+    std::stringstream in{example};
+    const auto rules = input(in);
+
+    expect(std::size(rules) == 9_u);
+    expect(rules.at("light red").contents ==
+           contents_list_t{{1, "bright white"}, {2, "muted yellow"}});
+    expect(rules.at("dark orange").contents ==
+           contents_list_t{{3, "bright white"}, {4, "muted yellow"}});
+    expect(rules.at("bright white").contents ==
+           contents_list_t{{1, "shiny gold"}});
+    expect(rules.at("muted yellow").contents ==
+           contents_list_t{{2, "shiny gold"}, {9, "faded blue"}});
+    expect(rules.at("shiny gold").contents ==
+           contents_list_t{{1, "dark olive"}, {2, "vibrant plum"}});
+    // ...
+    expect(rules.at("dotted black").contents == contents_list_t{});
+  };
 }
