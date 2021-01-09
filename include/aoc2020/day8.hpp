10d234cbfac12d546250583d2fd3859d7e06054c
Ben Goodwyn <bengoodwyn@me.com>
2021-01-09 12:13:03 -0600

    :new: [day8] Stub day 8 file structure

---
+++ include/aoc2020/day8.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <istream>
+
+namespace aoc2020::day8 {
+
+auto input(std::istream &) { return 0; }
+
+auto part1(const auto &) { return "stub"; }
+
+auto part2(const auto &) { return "stub"; }
+
+} // namespace aoc2020::day8
---
+++ src/day8_test.cpp
@@ -0,0 +1,21 @@
+#include "aoc2020/day8.hpp"
+
+#include <boost/ut.hpp>
+
+const std::string example_text{
+    R"(nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6)"};
+
+int main() {
+  using namespace aoc2020::day8;
+  using namespace boost::ut;
+
+  "stub"_test = [&] {};
+}
