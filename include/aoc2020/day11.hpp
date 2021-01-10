70cc0c04d0a4160747796f8b73d9d2ca3eccca61
Ben Goodwyn <bengoodwyn@me.com>
2021-01-10 12:26:09 -0600

    :arrow_right: [day11] Parse input for day 11

--- include/aoc2020/day11.hpp
+++ include/aoc2020/day11.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <istream>
-
+#include <vector>
 namespace aoc2020::day11 {
 
-auto input(std::istream &) { return 0; }
+auto input(std::istream &in) {
+  std::vector<std::string> result{};
+  std::string line{};
+  while (std::getline(in, line)) {
+    result.push_back(line);
+  }
+  return result;
+}
 
 auto part1(std::istream &) { return "stub"; }
 
--- src/day11_test.cpp
+++ src/day11_test.cpp
@@ -3,8 +3,10 @@
 #include <boost/ut.hpp>
 
 #include <sstream>
+#include <string>
+#include <vector>
 
-const std::string example{
+const std::vector<std::string> examples{
     R"(L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
@@ -14,11 +16,68 @@ L.LLLLL.LL
 ..L.L.....
 LLLLLLLLLL
 L.LLLLLL.L
-L.LLLLL.LL)"};
+L.LLLLL.LL)",
+    R"(#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##)",
+    R"(#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##)",
+    R"(#.##.L#.##
+#L###LL.L#
+L.#.#..#..
+#L##.##.L#
+#.##.LL.LL
+#.###L#.##
+..#.#.....
+#L######L#
+#.LL###L.L
+#.#L###.##)",
+    R"(#.#L.L#.##
+#LLL#LL.L#
+L.L.L..#..
+#LLL.##.L#
+#.LL.LL.LL
+#.LL#L#.##
+..L.L.....
+#L#LLLL#L#
+#.LLLLLL.L
+#.#L#L#.##)",
+    R"(#.#L.L#.##
+#LLL#LL.L#
+L.#.L..#..
+#L##.##.L#
+#.#L.LL.LL
+#.#L#L#.##
+..L.L.....
+#L#L##L#L#
+#.LLLLLL.L
+#.#L#L#.##)"};
 
 int main() {
   using namespace aoc2020::day11;
   using namespace boost::ut;
 
-  "stub"_test = [&] {};
+  "input"_test = [&] {
+    std::stringstream in{examples[0]};
+    const auto result = input(in);
+    expect(std::vector<std::string>{"L.LL.LL.LL", "LLLLLLL.LL", "L.L.L..L..",
+                                    "LLLL.LL.LL", "L.LL.LL.LL", "L.LLLLL.LL",
+                                    "..L.L.....", "LLLLLLLLLL", "L.LLLLLL.L",
+                                    "L.LLLLL.LL"} == result);
+  };
 }
