86b75408c946953e9ef46ac14fc62fa619f5a79a
Ben Goodwyn <bengoodwyn@me.com>
2021-01-10 14:35:47 -0600

    :new: [day12] Stub day 12 file structure

---
+++ include/aoc2020/day12.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <istream>
+
+namespace aoc2020::day12 {
+
+auto part1(std::istream &) { return 0; }
+
+auto part2(std::istream &) { return 0; }
+
+} // namespace aoc2020::day12
---
+++ src/day12_test.cpp
@@ -0,0 +1,30 @@
+#include "aoc2020/day12.hpp"
+
+#include <boost/ut.hpp>
+
+#include <sstream>
+#include <string>
+
+const std::string example{
+    R"(F10
+N3
+F7
+R90
+F11)"};
+
+int main() {
+  using namespace aoc2020::day12;
+  using namespace boost::ut;
+
+  "part1"_test = [&] {
+    std::stringstream in{example};
+    const auto result = part1(in);
+    expect(0_i == result);
+  };
+
+  "part2"_test = [&] {
+    std::stringstream in{example};
+    const auto result = part2(in);
+    expect(0_i == result);
+  };
+}
