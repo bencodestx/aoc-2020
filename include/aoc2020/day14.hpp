c3ec6aa42c6d3938a7b2aaf8b492eef394c5a3eb
Ben Goodwyn <bengoodwyn@me.com>
2021-01-18 12:58:24 -0600

    :art: [day14] Extract sum and mask operations to a common vm

--- include/aoc2020/day14.hpp
+++ include/aoc2020/day14.hpp
@@ -9,30 +9,38 @@
 
 namespace aoc2020::day14 {
 
-struct vm_v1_t {
+struct vm_t {
   std::bitset<64> is_1{};
   std::bitset<64> is_x{};
   std::unordered_map<uint64_t, uint64_t> memory{};
+
+  void mask(const std::string_view arg) {
+    is_1.reset();
+    is_x.reset();
+    size_t index = 0;
+    for (auto it = std::rbegin(arg); std::rend(arg) != it; it++) {
+      switch (*it) {
+      case 'X':
+        is_x[index] = true;
+        break;
+      case '1':
+        is_1[index] = true;
+        break;
+      }
+      ++index;
+    }
+  }
+
+  uint64_t sum_memory() const {
+    uint64_t sum{0};
+    for (const auto &[address, value] : memory) {
+      sum += value;
+    }
+    return sum;
+  }
 };
 
-void mask(auto &vm, const std::string_view arg) {
-  vm.is_1.reset();
-  vm.is_x.reset();
-  size_t index = 0;
-  for (auto it = std::rbegin(arg); std::rend(arg) != it; it++) {
-    switch (*it) {
-    case 'X':
-      vm.is_x[index] = true;
-      break;
-    case '1':
-      vm.is_1[index] = true;
-      break;
-    }
-    ++index;
-  }
-}
-
-void mem(vm_v1_t &vm, const std::string op, const std::string arg) {
+void mem_v1(auto &vm, const std::string op, const std::string arg) {
   const uint64_t addr = std::stoul(op.substr(4, std::size(op) - 1));
   const std::bitset<64> value = std::stoul(arg);
 
@@ -43,35 +51,25 @@ auto part1(std::istream &in) {
   std::string op;
   std::string equals;
   std::string arg;
-  vm_v1_t vm{};
+  vm_t vm{};
 
   while (in >> op >> equals >> arg) {
     if (equals != "=") {
       throw std::runtime_error(std::string{"Expecting '=': "} + equals);
     }
     if (op == "mask") {
-      mask(vm, arg);
+      vm.mask(arg);
     } else if (0u == op.find("mem[")) {
-      mem(vm, op, arg);
+      mem_v1(vm, op, arg);
     } else {
       throw std::runtime_error(std::string{"Unknown op: "} + op);
     }
   }
 
-  uint64_t sum{0};
-  for (const auto &[k, v] : vm.memory) {
-    sum += v;
-  }
-  return sum;
+  return vm.sum_memory();
 }
 
-struct vm_v2_t {
-  std::bitset<64> is_x{};
-  std::bitset<64> is_1{};
-  std::unordered_map<uint64_t, uint64_t> memory{};
-};
-
-void mem(vm_v2_t &vm, const std::string op, const std::string arg) {
+void mem_v2(auto &vm, const std::string op, const std::string arg) {
   std::bitset<64> base_addr = std::stoul(op.substr(4, std::size(op) - 1));
   const uint64_t value = std::stoul(arg);
 
@@ -108,26 +106,22 @@ auto part2(std::istream &in) {
   std::string op;
   std::string equals;
   std::string arg;
-  vm_v2_t vm{};
+  vm_t vm{};
 
   while (in >> op >> equals >> arg) {
     if (equals != "=") {
       throw std::runtime_error(std::string{"Expecting '=': "} + equals);
     }
     if (op == "mask") {
-      mask(vm, arg);
+      vm.mask(arg);
     } else if (0u == op.find("mem[")) {
-      mem(vm, op, arg);
+      mem_v2(vm, op, arg);
     } else {
       throw std::runtime_error(std::string{"Unknown op: "} + op);
     }
   }
 
-  uint64_t sum{0};
-  for (const auto &[k, v] : vm.memory) {
-    sum += v;
-  }
-  return sum;
+  return vm.sum_memory();
 }
 
 } // namespace aoc2020::day14
