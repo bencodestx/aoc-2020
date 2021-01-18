9fd752760e4897eb859d7f066db34b5c232d5a09
Ben Goodwyn <bengoodwyn@me.com>
2021-01-18 13:05:26 -0600

    :art: [day14] Migrating towards a policy-based design

--- include/aoc2020/day14.hpp
+++ include/aoc2020/day14.hpp
@@ -9,7 +9,7 @@
 
 namespace aoc2020::day14 {
 
-struct vm_t {
+template <typename TUpdateMemoryPolicy> struct vm_t {
   std::bitset<64> is_1{};
   std::bitset<64> is_x{};
   std::unordered_map<uint64_t, uint64_t> memory{};
@@ -31,6 +31,11 @@ struct vm_t {
     }
   }
 
+  void mem(const std::string op, const std::string arg) {
+    TUpdateMemoryPolicy::mem(*this, std::stoul(op.substr(4, std::size(op) - 1)),
+                             std::stoul(arg));
+  }
+
   uint64_t sum_memory() const {
     uint64_t sum{0};
     for (const auto &[address, value] : memory) {
@@ -40,18 +45,19 @@ struct vm_t {
   }
 };
 
-void mem_v1(auto &vm, const std::string op, const std::string arg) {
-  const uint64_t addr = std::stoul(op.substr(4, std::size(op) - 1));
-  const std::bitset<64> value = std::stoul(arg);
+struct part1_policy {
+  static void mem(auto &vm, const uint64_t addr, const uint64_t val) {
+    const std::bitset<64> value{val};
 
-  vm.memory[addr] = (vm.is_1 | (value & vm.is_x)).to_ulong();
-}
+    vm.memory[addr] = (vm.is_1 | (value & vm.is_x)).to_ulong();
+  }
+};
 
 auto part1(std::istream &in) {
   std::string op;
   std::string equals;
   std::string arg;
-  vm_t vm{};
+  vm_t<part1_policy> vm{};
 
   while (in >> op >> equals >> arg) {
     if (equals != "=") {
@@ -60,7 +66,7 @@ auto part1(std::istream &in) {
     if (op == "mask") {
       vm.mask(arg);
     } else if (0u == op.find("mem[")) {
-      mem_v1(vm, op, arg);
+      vm.mem(op, arg);
     } else {
       throw std::runtime_error(std::string{"Unknown op: "} + op);
     }
@@ -69,44 +75,44 @@ auto part1(std::istream &in) {
   return vm.sum_memory();
 }
 
-void mem_v2(auto &vm, const std::string op, const std::string arg) {
-  std::bitset<64> base_addr = std::stoul(op.substr(4, std::size(op) - 1));
-  const uint64_t value = std::stoul(arg);
+struct part2_policy {
+  static void mem(auto &vm, const uint64_t addr, const uint64_t value) {
+    std::bitset<64> base_addr{addr};
+    std::vector<size_t> floating_bit_offsets{};
 
-  std::vector<size_t> floating_bit_offsets{};
+    for (size_t index = 0; index < 64; ++index) {
+      if (vm.is_1[index]) {
+        base_addr[index] = true;
+      } else if (vm.is_x[index]) {
+        floating_bit_offsets.push_back(index);
+      }
+    }
 
-  for (size_t index = 0; index < 64; ++index) {
-    if (vm.is_1[index]) {
-      base_addr[index] = true;
-    } else if (vm.is_x[index]) {
-      floating_bit_offsets.push_back(index);
+    for (uint64_t floating_value = 0;
+         floating_value < (1ul << std::size(floating_bit_offsets));
+         ++floating_value) {
+      auto floating_addr = base_addr;
+      const std::bitset<64> floating_value_bits{floating_value};
+      for (size_t floating_bit_index = 0;
+           floating_bit_index < std::size(floating_bit_offsets);
+           ++floating_bit_index) {
+        floating_addr[floating_bit_offsets[floating_bit_index]] =
+            floating_value_bits[floating_bit_index];
+      }
+      if (0 == value) {
+        vm.memory.erase(floating_addr.to_ulong());
+      } else {
+        vm.memory[floating_addr.to_ulong()] = value;
+      }
     }
   }
-
-  for (uint64_t floating_value = 0;
-       floating_value < (1ul << std::size(floating_bit_offsets));
-       ++floating_value) {
-    auto floating_addr = base_addr;
-    const std::bitset<64> floating_value_bits{floating_value};
-    for (size_t floating_bit_index = 0;
-         floating_bit_index < std::size(floating_bit_offsets);
-         ++floating_bit_index) {
-      floating_addr[floating_bit_offsets[floating_bit_index]] =
-          floating_value_bits[floating_bit_index];
-    }
-    if (0 == value) {
-      vm.memory.erase(floating_addr.to_ulong());
-    } else {
-      vm.memory[floating_addr.to_ulong()] = value;
-    }
-  }
-}
+};
 
 auto part2(std::istream &in) {
   std::string op;
   std::string equals;
   std::string arg;
-  vm_t vm{};
+  vm_t<part2_policy> vm{};
 
   while (in >> op >> equals >> arg) {
     if (equals != "=") {
@@ -115,7 +121,7 @@ auto part2(std::istream &in) {
     if (op == "mask") {
       vm.mask(arg);
     } else if (0u == op.find("mem[")) {
-      mem_v2(vm, op, arg);
+      vm.mem(op, arg);
     } else {
       throw std::runtime_error(std::string{"Unknown op: "} + op);
     }
