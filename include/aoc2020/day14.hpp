e3d0c041da3de3bcfbf4d3dd8b983901d02ff898
Ben Goodwyn <bengoodwyn@me.com>
2021-01-18 12:53:28 -0600

    :art: [day14] Make part 1 and part 2 more similar in structure

--- include/aoc2020/day14.hpp
+++ include/aoc2020/day14.hpp
@@ -10,29 +10,23 @@
 namespace aoc2020::day14 {
 
 struct vm_v1_t {
-  std::bitset<64> values{};
-  std::bitset<64> bitmask{};
-  std::unordered_map<uint64_t, std::bitset<64>> memory{};
+  std::bitset<64> is_1{};
+  std::bitset<64> is_x{};
+  std::unordered_map<uint64_t, uint64_t> memory{};
 };
 
-void mask(vm_v1_t &vm, const std::string_view arg) {
+void mask(auto &vm, const std::string_view arg) {
+  vm.is_1.reset();
+  vm.is_x.reset();
   size_t index = 0;
   for (auto it = std::rbegin(arg); std::rend(arg) != it; it++) {
     switch (*it) {
     case 'X':
-      vm.bitmask[index] = true;
-      vm.values[index] = false;
-      break;
-    case '0':
-      vm.bitmask[index] = false;
-      vm.values[index] = false;
+      vm.is_x[index] = true;
       break;
     case '1':
-      vm.bitmask[index] = false;
-      vm.values[index] = true;
+      vm.is_1[index] = true;
       break;
-    default:
-      throw std::runtime_error(std::string{"Invalid bitmask: "} + *it);
     }
     ++index;
   }
@@ -42,7 +36,7 @@ void mem(vm_v1_t &vm, const std::string op, const std::string arg) {
   const uint64_t addr = std::stoul(op.substr(4, std::size(op) - 1));
   const std::bitset<64> value = std::stoul(arg);
 
-  vm.memory[addr] = vm.values | (value & vm.bitmask);
+  vm.memory[addr] = (vm.is_1 | (value & vm.is_x)).to_ulong();
 }
 
 auto part1(std::istream &in) {
@@ -66,37 +60,16 @@ auto part1(std::istream &in) {
 
   uint64_t sum{0};
   for (const auto &[k, v] : vm.memory) {
-    sum += v.to_ulong();
+    sum += v;
   }
   return sum;
 }
 
 struct vm_v2_t {
-  std::bitset<64> force_on{};
-  std::bitset<64> floating{};
-  std::unordered_map<std::bitset<64>, uint64_t> memory{};
+  std::bitset<64> is_x{};
+  std::bitset<64> is_1{};
+  std::unordered_map<uint64_t, uint64_t> memory{};
 };
-
-void mask(vm_v2_t &vm, const std::string_view arg) {
-  size_t index = 0;
-  vm.force_on.reset();
-  vm.floating.reset();
-  for (auto it = std::rbegin(arg); std::rend(arg) != it; it++) {
-    switch (*it) {
-    case 'X':
-      vm.floating[index] = true;
-      break;
-    case '0':
-      break;
-    case '1':
-      vm.force_on[index] = true;
-      break;
-    default:
-      throw std::runtime_error(std::string{"Invalid bitmask: "} + *it);
-    }
-    ++index;
-  }
-}
 
 void mem(vm_v2_t &vm, const std::string op, const std::string arg) {
   std::bitset<64> base_addr = std::stoul(op.substr(4, std::size(op) - 1));
@@ -105,9 +78,9 @@ void mem(vm_v2_t &vm, const std::string op, const std::string arg) {
   std::vector<size_t> floating_bit_offsets{};
 
   for (size_t index = 0; index < 64; ++index) {
-    if (vm.force_on[index]) {
+    if (vm.is_1[index]) {
       base_addr[index] = true;
-    } else if (vm.floating[index]) {
+    } else if (vm.is_x[index]) {
       floating_bit_offsets.push_back(index);
     }
   }
