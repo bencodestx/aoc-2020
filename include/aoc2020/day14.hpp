493570d129551e3e351f45f256db0ce88c09685d
Ben Goodwyn <bengoodwyn@me.com>
2021-01-18 12:43:11 -0600

    :checkered_flag: [day14] Completed day 14 part 2

--- include/aoc2020/day14.hpp
+++ include/aoc2020/day14.hpp
@@ -2,20 +2,20 @@
 
 #include <bitset>
 #include <cstdint>
-#include <iostream>
 #include <istream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace aoc2020::day14 {
 
-struct vm_t {
+struct vm_v1_t {
   std::bitset<64> values{};
   std::bitset<64> bitmask{};
   std::unordered_map<uint64_t, std::bitset<64>> memory{};
 };
 
-void mask(vm_t &vm, const std::string_view arg) {
+void mask(vm_v1_t &vm, const std::string_view arg) {
   size_t index = 0;
   for (auto it = std::rbegin(arg); std::rend(arg) != it; it++) {
     switch (*it) {
@@ -38,7 +38,7 @@ void mask(vm_t &vm, const std::string_view arg) {
   }
 }
 
-void mem(vm_t &vm, const std::string op, const std::string arg) {
+void mem(vm_v1_t &vm, const std::string op, const std::string arg) {
   const uint64_t addr = std::stoul(op.substr(4, std::size(op) - 1));
   const std::bitset<64> value = std::stoul(arg);
 
@@ -49,9 +49,7 @@ auto part1(std::istream &in) {
   std::string op;
   std::string equals;
   std::string arg;
-  vm_t vm{};
-
-  std::cerr << sizeof(unsigned long) << std::endl;
+  vm_v1_t vm{};
 
   while (in >> op >> equals >> arg) {
     if (equals != "=") {
@@ -73,6 +71,90 @@ auto part1(std::istream &in) {
   return sum;
 }
 
-auto part2(std::istream &) { return 0u; }
+struct vm_v2_t {
+  std::bitset<64> force_on{};
+  std::bitset<64> floating{};
+  std::unordered_map<std::bitset<64>, uint64_t> memory{};
+};
+
+void mask(vm_v2_t &vm, const std::string_view arg) {
+  size_t index = 0;
+  vm.force_on.reset();
+  vm.floating.reset();
+  for (auto it = std::rbegin(arg); std::rend(arg) != it; it++) {
+    switch (*it) {
+    case 'X':
+      vm.floating[index] = true;
+      break;
+    case '0':
+      break;
+    case '1':
+      vm.force_on[index] = true;
+      break;
+    default:
+      throw std::runtime_error(std::string{"Invalid bitmask: "} + *it);
+    }
+    ++index;
+  }
+}
+
+void mem(vm_v2_t &vm, const std::string op, const std::string arg) {
+  std::bitset<64> base_addr = std::stoul(op.substr(4, std::size(op) - 1));
+  const uint64_t value = std::stoul(arg);
+
+  std::vector<size_t> floating_bit_offsets{};
+
+  for (size_t index = 0; index < 64; ++index) {
+    if (vm.force_on[index]) {
+      base_addr[index] = true;
+    } else if (vm.floating[index]) {
+      floating_bit_offsets.push_back(index);
+    }
+  }
+
+  for (uint64_t floating_value = 0;
+       floating_value < (1ul << std::size(floating_bit_offsets));
+       ++floating_value) {
+    auto floating_addr = base_addr;
+    const std::bitset<64> floating_value_bits{floating_value};
+    for (size_t floating_bit_index = 0;
+         floating_bit_index < std::size(floating_bit_offsets);
+         ++floating_bit_index) {
+      floating_addr[floating_bit_offsets[floating_bit_index]] =
+          floating_value_bits[floating_bit_index];
+    }
+    if (0 == value) {
+      vm.memory.erase(floating_addr.to_ulong());
+    } else {
+      vm.memory[floating_addr.to_ulong()] = value;
+    }
+  }
+}
+
+auto part2(std::istream &in) {
+  std::string op;
+  std::string equals;
+  std::string arg;
+  vm_v2_t vm{};
+
+  while (in >> op >> equals >> arg) {
+    if (equals != "=") {
+      throw std::runtime_error(std::string{"Expecting '=': "} + equals);
+    }
+    if (op == "mask") {
+      mask(vm, arg);
+    } else if (0u == op.find("mem[")) {
+      mem(vm, op, arg);
+    } else {
+      throw std::runtime_error(std::string{"Unknown op: "} + op);
+    }
+  }
+
+  uint64_t sum{0};
+  for (const auto &[k, v] : vm.memory) {
+    sum += v;
+  }
+  return sum;
+}
 
 } // namespace aoc2020::day14
--- src/day14_test.cpp
+++ src/day14_test.cpp
@@ -4,23 +4,29 @@
 
 #include <sstream>
 
-const std::string example{
+const std::string example_v1{
     R"(mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0)"};
+
+const std::string example_v2{
+    R"(mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1)"};
 
 int main() {
   using namespace aoc2020::day14;
   using namespace boost::ut;
 
   "part1"_test = [&] {
-    std::stringstream in{example};
+    std::stringstream in{example_v1};
     expect(165_u == part1(in));
   };
 
   "part2"_test = [&] {
-    std::stringstream in{example};
-    expect(0_u == part2(in));
+    std::stringstream in{example_v2};
+    expect(208_u == part2(in));
   };
 }
