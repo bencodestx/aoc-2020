227362badbafc2fa8c55a4d222892a095dfaf231
Ben Goodwyn <bengoodwyn@me.com>
2021-01-18 11:36:51 -0600

    :new: [day14] Stubs for day 14

---
+++ src/day14_part1.cpp
@@ -0,0 +1,11 @@
+#include "aoc2020/day14.hpp"
+
+#include <iostream>
+
+int main() {
+  using namespace aoc2020::day14;
+
+  std::cout << part1(std::cin) << std::endl;
+
+  return {};
+}
---
+++ src/day14_test.cpp
@@ -0,0 +1,26 @@
+#include "aoc2020/day14.hpp"
+
+#include <boost/ut.hpp>
+
+#include <sstream>
+
+const std::string example{
+    R"(mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0)"};
+
+int main() {
+  using namespace aoc2020::day14;
+  using namespace boost::ut;
+
+  "part1"_test = [&] {
+    std::stringstream in{example};
+    expect(0_u == part1(in));
+  };
+
+  "part2"_test = [&] {
+    std::stringstream in{example};
+    expect(0_u == part2(in));
+  };
+}
