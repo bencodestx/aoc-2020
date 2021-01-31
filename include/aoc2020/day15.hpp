e29f7152082a1185cea4f7bdfd186c869c825c9d
Ben Goodwyn <bengoodwyn@me.com>
2021-01-31 12:15:51 -0600

    :checkered_flag: [day15] Completed day 15 part 1

--- include/aoc2020/day15.hpp
+++ include/aoc2020/day15.hpp
@@ -1,8 +1,46 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
 namespace aoc2020::day15 {
 
-auto part1(auto &) { return 0u; }
+struct memory_game {
+  size_t current_turn{1u};
+  size_t previous_turn_of_last_spoken_number{0u};
+  std::unordered_map<size_t, size_t> number_to_previous_turn{};
+
+  auto speak(size_t number) {
+    auto &previous_turn = number_to_previous_turn[number];
+    previous_turn_of_last_spoken_number = previous_turn;
+    previous_turn = current_turn++;
+    return number;
+  }
+
+  auto speak_next() {
+    if (0 == previous_turn_of_last_spoken_number) {
+      return speak(0u);
+    } else {
+      return speak(current_turn - 1u - previous_turn_of_last_spoken_number);
+    }
+  }
+};
+
+auto part1(auto &in, size_t turns = 2020u) {
+  memory_game game{};
+  size_t number;
+  while (in >> number) {
+    game.speak(number);
+    char comma;
+    if (not(in >> comma)) {
+      break;
+    }
+  }
+  while (game.current_turn <= turns) {
+    number = game.speak_next();
+  }
+  return number;
+}
 
 auto part2(auto &) { return 0u; }
 
--- src/day15_test.cpp
+++ src/day15_test.cpp
@@ -8,10 +8,61 @@ int main() {
   using namespace aoc2020::day15;
   using namespace boost::ut;
 
-  "part1"_test = [&] {
-    std::stringstream in{};
-    expect(0_u == part1(in));
+  "For example, suppose the starting numbers are 0,3,6"_test = [&] {
+    memory_game game{};
+    game.speak(0u);
+    game.speak(3u);
+    game.speak(6u);
+
+    expect(0_u == game.speak_next());
+    expect(3_u == game.speak_next());
+    expect(3_u == game.speak_next());
+    expect(1_u == game.speak_next());
+    expect(0_u == game.speak_next());
+    expect(4_u == game.speak_next());
+    expect(0_u == game.speak_next());
   };
+
+  "Given the starting numbers 0,3,6, the 10th number spoken is 0."_test = [&] {
+    std::stringstream in{"0,3,6"};
+    expect(0_u == part1(in, 10u));
+  };
+
+  "Given the starting numbers 1,3,2, the 2020th number spoken is 1."_test =
+      [&] {
+        std::stringstream in{"1,3,2"};
+        expect(1_u == part1(in));
+      };
+
+  "Given the starting numbers 2,1,3, the 2020th number spoken is 1."_test =
+      [&] {
+        std::stringstream in{"2,1,3"};
+        expect(10_u == part1(in));
+      };
+
+  "Given the starting numbers 1,2,3, the 2020th number spoken is 1."_test =
+      [&] {
+        std::stringstream in{"1,2,3"};
+        expect(27_u == part1(in));
+      };
+
+  "Given the starting numbers 2,3,1, the 2020th number spoken is 1."_test =
+      [&] {
+        std::stringstream in{"2,3,1"};
+        expect(78_u == part1(in));
+      };
+
+  "Given the starting numbers 3,2,1, the 2020th number spoken is 1."_test =
+      [&] {
+        std::stringstream in{"3,2,1"};
+        expect(438_u == part1(in));
+      };
+
+  "Given the starting numbers 3,1,2, the 2020th number spoken is 1."_test =
+      [&] {
+        std::stringstream in{"3,1,2"};
+        expect(1836_u == part1(in));
+      };
 
   "part2"_test = [&] {
     std::stringstream in{};
